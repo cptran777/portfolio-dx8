fe26f77b234b24926605375ac9a30e77b62051aa
cptran777 <cptran14@gmail.com>
2017-03-02 02:00:38 -0800

    add code to be able to expand and collapse top banner

--- app/components/index/main-container.js
+++ app/components/index/main-container.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  classNameBindings: ['isCoverExpanded:show-cover', 'isCoverHidden:hide-cover'],
+
   /**
    * Injected Ember service to track the state of the navbar
    * @type {Object}
@@ -17,11 +20,43 @@ export default Ember.Component.extend({
   }),
 
   /**
+   * Boolean for whether or not the cover is expanded
+   * @type {Boolean}
+   */
+  isCoverExpanded: false,
+
+  /**
+   * Boolean that is the opposite of isCoverExpanded
+   * @return {Boolean}
+   */
+  isCoverHidden: Ember.computed.not('isCoverExpanded'),
+
+  /**
    * Listens for change in the isCollapsed property and sets the page header
    * class accordingly
    * @return {String}
    */
-  pageHeaderClass: Ember.computed('isCollapsed', function() {
-    return this.get('isCollapsed') ? 'header-collapsed' : 'header-expanded';
-  })
+  pageHeaderClass: Ember.computed('isCollapsed', 'isCoverExpanded', function() {
+    const {
+      isCollapsed,
+      isCoverExpanded
+    } = this.getProperties('isCollapsed', 'isCoverExpanded');
+    const classList = [];
+
+    classList.push(isCollapsed ? 'header-collapsed' : 'header-expanded');
+    classList.push(isCoverExpanded ? 'show-cover' : 'hide-cover');
+
+    return classList.join(' ');
+  }),
+
+  actions: {
+    /**
+     * Changes cover expanded state to modify the page header class accordingly
+     * @param  {Boolean}
+     * @return {undefined}
+     */
+    onChangeCover(isExpanded) {
+      this.set('isCoverExpanded', isExpanded);
+    }
+  }
 });
--- app/components/index/page-header.js
+++ app/components/index/page-header.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  /**
+   * Boolean for whether or not to expand banner to show the cover
+   * @type {Boolean}
+   */
+  showCover: false,
+
+  actions: {
+    onShowCover() {
+      this.set('showCover', true);
+      this.get('onChangeCover')(true);
+    },
+
+    onHideCover() {
+      this.set('showCover', false);
+      this.get('onChangeCover')(false);
+    }
+  }
 });
