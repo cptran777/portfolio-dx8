d82d0c6e72e84fe1a7019150f6ce8a093cf0e428
Charlie Tran <catran@linkedin.com>
2017-03-07 13:14:21 -0800

    config for createjs

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,9 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    createjs: {
+      import: ['preload', 'sound', 'easle', 'tween', 'webgl']
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
