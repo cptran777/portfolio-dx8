67e40ab4db4f1fd1ac655114c301e44ee78d5f9b
cptran777 <cptran14@gmail.com>
2017-08-01 20:22:27 -0700

    working on portfolio site again

---
+++ tests/unit/utils/static-content-test.js
@@ -0,0 +1,10 @@
+import staticContent from 'charlie-tran/utils/static-content';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | static content');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = staticContent();
+  assert.ok(result);
+});
