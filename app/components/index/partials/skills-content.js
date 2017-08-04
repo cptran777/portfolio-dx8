a53412eee0d043ea24a820336a624a953e430a70
cptran777 <cptran14@gmail.com>
2017-08-04 16:59:30 -0700

    add logic to reuse the skills content file for the currently working on file

--- app/components/index/partials/skills-content.js
+++ app/components/index/partials/skills-content.js
@@ -6,6 +6,9 @@
  * a reusable component unless the exact information were to happen to come up
  * elsewhere and is separated from the main-container purely for clean, modular
  * code
+ *
+ * Actually it will be kind of reusable. Will store static information to render on the page, but
+ * then use different flags in order to render the correct list of panels
  */
 import Ember from 'ember';
 
@@ -18,12 +21,14 @@ const basePath = 'assets/images/';
  * Filesnames of the images
  * @type {Array}
  */
+// These are specifically for the skills window
 const firstRowImages = ['logo-js.png', 'logo-react.png', 'logo-ember.png', 'logo-redux.png'];
 const secondRowImages = ['logo-html5.png', 'logo-css3.png', 'logo-jquery.gif', 'logo-d3.png'];
 const thirdRowImages = ['logo-threejs.png', 'logo-createjs.png', 'logo-angular.png', 'logo-bootstrap.png'];
 const fourthRowImages = ['logo-node.png', 'logo-express.png', 'logo-websocket.png', 'logo-webrtc.png'];
 const fifthRowImages = ['logo-mysql.png', 'logo-redis.png', 'logo-postgres.png', 'logo-mongodb.png'];
-const sixthRowImages = [];
+// These are for the currently working on window
+const workingOnFirstRow = ['logo-python.png', 'logo-c.png', 'logo-csharp.png', 'logo-unity.png'];
 
 /**
  * Helper function to be passed into the rows that appends the base path to the image file
@@ -42,20 +47,28 @@ export default Ember.Component.extend({
   thirdRowImages: thirdRowImages.map(appendBase),
   fourthRowImages: fourthRowImages.map(appendBase),
   fifthRowImages: fifthRowImages.map(appendBase),
-  sixthRowImages: sixthRowImages.map(appendBase),
+  // These are for the currently working on items
+  workingOnFirstRow: workingOnFirstRow.map(appendBase),
 
   /**
    * Groups all of the rows of images together for easy rendering
    * @type {Array}
    */
-  rowsOfImages: Ember.computed(function() {
-    return [
-      this.get('firstRowImages'),
-      this.get('secondRowImages'),
-      this.get('thirdRowImages'),
-      this.get('fourthRowImages'),
-      this.get('fifthRowImages'),
-      this.get('sixthRowImages')
-    ];
+  rowsOfImages: Ember.computed('content', function() {
+    const content = this.get('content');
+
+    switch (content) {
+      case 'currentWorking':
+        return [ this.get('workingOnFirstRow') ];
+
+      default:
+        return [
+          this.get('firstRowImages'),
+          this.get('secondRowImages'),
+          this.get('thirdRowImages'),
+          this.get('fourthRowImages'),
+          this.get('fifthRowImages')
+        ];
+    }
   })
 });
--- app/components/index/partials/summary-content.js
+++ app/components/index/partials/summary-content.js
@@ -1,3 +1,10 @@
+/**
+ * components/index/partials/summary-content.js
+ *
+ * The summary content partials is merely a presentational component that renders the text
+ * for the summary content. It's simple, static content and does not need any special actions
+ * or properties in this related .js file
+ */
 import Ember from 'ember';
 
 export default Ember.Component.extend({
