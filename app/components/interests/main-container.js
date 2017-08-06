f42a6bc9b0b34d80f78ddbdd7e349bf44c9e95d3
cptran777 <cptran14@gmail.com>
2017-08-05 17:47:57 -0700

    move forward with interest page, refactor styling to give similar components to share route container

---
+++ app/components/interests/main-container.js
@@ -0,0 +1,8 @@
+/**
+ * Acts as the main component for the interests route. Handles the upper level
+ * actions that should affect multiple components
+ */
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+});
